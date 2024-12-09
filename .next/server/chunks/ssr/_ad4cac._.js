9a72386e8bfe467ca8aa5ce4ba735f0b4b989fc9
Hashim Hasan <141049257+asimvijay@users.noreply.github.com>
2024-12-09 16:09:26 +0500

    ok

--- .next/server/chunks/ssr/_ad4cac._.js
+++ .next/server/chunks/ssr/_ad4cac._.js
@@ -78,48 +78,6 @@ if (__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$app$2f$geistmono_b9f5
 }
 const __TURBOPACK__default__export__ = fontData;
 }}),
-"[project]/src/app/geistsans_9fc57718.js [app-rsc] (ecmascript)": ((__turbopack_context__) => {
-"use strict";
-
-var { r: __turbopack_require__, f: __turbopack_module_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, M: __turbopack_modules__, l: __turbopack_load__, j: __turbopack_dynamic__, P: __turbopack_resolve_absolute_path__, U: __turbopack_relative_url__, R: __turbopack_resolve_module_id_path__, b: __turbopack_worker_blob_url__, g: global, __dirname, x: __turbopack_external_require__, y: __turbopack_external_import__, z: require } = __turbopack_context__;
-{
-__turbopack_esm__({
-    "default": (()=>__TURBOPACK__default__export__)
-});
-var __TURBOPACK__imported__module__$5b$project$5d2f$src$2f$app$2f$geistsans_9fc57718$2e$module$2e$css__$5b$app$2d$client$5d$__$28$css__module$29$__ = __turbopack_import__("[project]/src/app/geistsans_9fc57718.module.css [app-client] (css module)");
-;
-const fontData = {
-    className: __TURBOPACK__imported__module__$5b$project$5d2f$src$2f$app$2f$geistsans_9fc57718$2e$module$2e$css__$5b$app$2d$client$5d$__$28$css__module$29$__["default"].className,
-    style: {
-        fontFamily: "'geistSans', 'geistSans Fallback'"
-    }
-};
-if (__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$app$2f$geistsans_9fc57718$2e$module$2e$css__$5b$app$2d$client$5d$__$28$css__module$29$__["default"].variable != null) {
-    fontData.variable = __TURBOPACK__imported__module__$5b$project$5d2f$src$2f$app$2f$geistsans_9fc57718$2e$module$2e$css__$5b$app$2d$client$5d$__$28$css__module$29$__["default"].variable;
-}
-const __TURBOPACK__default__export__ = fontData;
-}}),
-"[project]/src/app/geistmono_b9f59162.js [app-rsc] (ecmascript)": ((__turbopack_context__) => {
-"use strict";
-
-var { r: __turbopack_require__, f: __turbopack_module_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, M: __turbopack_modules__, l: __turbopack_load__, j: __turbopack_dynamic__, P: __turbopack_resolve_absolute_path__, U: __turbopack_relative_url__, R: __turbopack_resolve_module_id_path__, b: __turbopack_worker_blob_url__, g: global, __dirname, x: __turbopack_external_require__, y: __turbopack_external_import__, z: require } = __turbopack_context__;
-{
-__turbopack_esm__({
-    "default": (()=>__TURBOPACK__default__export__)
-});
-var __TURBOPACK__imported__module__$5b$project$5d2f$src$2f$app$2f$geistmono_b9f59162$2e$module$2e$css__$5b$app$2d$client$5d$__$28$css__module$29$__ = __turbopack_import__("[project]/src/app/geistmono_b9f59162.module.css [app-client] (css module)");
-;
-const fontData = {
-    className: __TURBOPACK__imported__module__$5b$project$5d2f$src$2f$app$2f$geistmono_b9f59162$2e$module$2e$css__$5b$app$2d$client$5d$__$28$css__module$29$__["default"].className,
-    style: {
-        fontFamily: "'geistMono', 'geistMono Fallback'"
-    }
-};
-if (__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$app$2f$geistmono_b9f59162$2e$module$2e$css__$5b$app$2d$client$5d$__$28$css__module$29$__["default"].variable != null) {
-    fontData.variable = __TURBOPACK__imported__module__$5b$project$5d2f$src$2f$app$2f$geistmono_b9f59162$2e$module$2e$css__$5b$app$2d$client$5d$__$28$css__module$29$__["default"].variable;
-}
-const __TURBOPACK__default__export__ = fontData;
-}}),
 "[project]/src/app/layout.tsx [app-rsc] (ecmascript)": ((__turbopack_context__) => {
 "use strict";
 
@@ -190,8 +148,8 @@ function RootLayout({ children }) {
                     }, this),
                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("link", {
                         rel: "icon",
-                        href: "/favicon.ico",
-                        type: "image/x-icon"
+                        href: "/images/me.jpg",
+                        type: "image/jpg"
                     }, void 0, false, {
                         fileName: "[project]/src/app/layout.tsx",
                         lineNumber: 58,
@@ -260,4 +218,4 @@ if ((typeof exports.default === 'function' || typeof exports.default === 'object
 
 };
 
-//# sourceMappingURL=_8ae8b0._.js.map
+//# sourceMappingURL=_ad4cac._.js.map
